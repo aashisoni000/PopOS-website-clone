a86df4d12b4615d0dd2765112bfb633d94334c3c
Aashi <aashi2701soni@gmail.com>
2025-09-14 03:03:27 +0530

    added navbar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
-  const sidebar = document.getElementById('sidebar');
-  const closeBtn = document.querySelector('.close-btn');
+const overlay = document.getElementById('overlay');
+const closeBtn = document.querySelector('.close-btn');
 
-  hamburger.addEventListener('click', () => {
-    sidebar.classList.add('active');
-  });
+hamburger.addEventListener('click', () => {
+  overlay.classList.add('active');
+});
 
-  closeBtn.addEventListener('click', () => {
-    sidebar.classList.remove('active');
-  });
+closeBtn.addEventListener('click', () => {
+  overlay.classList.remove('active');
+});
